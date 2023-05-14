a03be7b7722889edfc7a9d7e119db25d23a0db15
Kilvio Lluberes <kilviolluberes@Kilvios-MacBook-Air.local>
2023-05-14 08:31:40 -0400

    resync

--- medusa-config.js
+++ medusa-config.js
@@ -37,11 +37,12 @@ const DB_HOST = process.env.DB_HOST
 const DB_PORT = process.env.DB_PORT
 const DB_DATABASE = process.env.DB_DATABASE
 
+/*
 const DATABASE_URL = 
   `postgres://${DB_USERNAME}:${DB_PASSWORD}` +
-  `@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`
+  `@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`*/
 
-// *OLD* process.env.DATABASE_URL || "postgresql://postgres:mac@localhost:5432/medusadb";
+const DATABASE_URL = process.env.DATABASE_URL || "postgresql://postgres:mac@localhost:5432/medusadb";
 
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
